1ff39a97a50965da04dcc63bdc510a538fd2b7e6
KevinRoost <kroost92@gmail.com>
2023-02-04 17:42:43 -0500

    create profile model

--- models/profile.js
+++ models/profile.js
@@ -1,11 +1,23 @@
 import mongoose from 'mongoose'
+import User from './user.js'
 
 const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
   name: String,
-  photo: String
-},{
+  photo: String,
+  pronouns: {
+    type: String,
+    enum: ['He/Him/His', 'She/Her/Hers', 'They/Them/Theirs']
+  },
+  cd: Boolean,//true = cd, false = talent
+  location: String,//where the user is based
+  phoneNumber: Number,
+  email: String,
+  website: String,
+  talentAccount: { type: Schema.Types.ObjectId, ref: 'ActorAccount'},
+  cdAccount: { type: Schema.Types.ObjectId, ref: 'CDAccount'}
+}, {
   timestamps: true,
 })
 
