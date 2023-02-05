999a2c951ed1a3c3caee7f00e27a0297f3e5e9cd
KevinRoost <kroost92@gmail.com>
2023-02-05 00:28:08 -0500

    create embedded listSchema

--- models/cdAccount.js
+++ models/cdAccount.js
@@ -3,6 +3,13 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const listSchema = new Schema({
+  titleOfList: String,
+  talent: { type: Schema.Types.ObjectId, ref: 'TalentAccount' },
+  notes: String
+}, {
+  timestamps: true
+})
 
 const cdAccountSchema = new Schema({
   company: String,//producing entity that the CD represents
